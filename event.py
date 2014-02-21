daca6da934969f9d10e3e724ffabcff5f33b3dd7
Peter Dawson <petedaws@gmail.com>
2014-02-21 20:25:24 +1000

    made the timer modifyable

--- event.py
+++ event.py
@@ -5,17 +5,17 @@ import time
 class Event:
 
 	def __init__(self):
-		self.events = {}
+		self.events = []
 		self.timers = []
 
-	def emit(self,event_name):
+	def emit(self,event_name,*args):
 		for event in self.events:
 			if event['name'] == event_name:
-				self.events[event]()
+				event['callback'](*args)
 
 	def connect(self,event_name,callback):
 		assert callable(callback)
-		self.events[event] = callback
+		self.events.append({'name':event_name,'callback':callback})
 
 	def timer_add(self,event_name,period):
 		self.timers.append[(event_name,period)]
@@ -28,9 +28,18 @@ timers = []
 read_fds = {}
 write_fds = []
 
-def add_timer(interval,callback):
+def add_timer(interval,callback,name=''):
 	next = time.time() + interval
-	timers.append({'interval':interval,'callback':callback})
+	timers.append({'name':name,'interval':interval,'callback':callback})
+
+def modify_timer(name,interval=None,callback=None):
+	for timer in timers:
+		if timer['name'] == name:
+			if interval is not None:
+				timer['interval'] = interval
+			if callback is not None:
+				timer['callback'] = callback
+	
 
 def add_io_watcher(fd,callback):
 	read_fds[fd] = callback
@@ -44,6 +53,8 @@ def mainloop():
 		next_timer = None
 		timeout = None
 		for timer in timers:
+			if timer['interval'] == 0:
+				continue
 			if next_timer is None or timer['next'] < next_timer['next']:
 				next_timer = timer
 				timeout = next_timer['next'] - time.time()
@@ -51,6 +62,8 @@ def mainloop():
 		readable, writeable, exceptional = select.select(read_fds.keys(), write_fds, read_fds,max(timeout,0))
 
 		for timer in timers:
+			if timer['interval'] == 0:
+				continue
 			if timer['next'] - time.time() <= 0:
 				timer['callback']()
 				timer['next'] = time.time() + timer['interval']
--- test_event.py
+++ test_event.py
@@ -6,14 +6,24 @@ import struct
 class TimerTest(event.Event):
 	
 	def __init__(self,hellostring='helloworld'):
+		event.Event.__init__(self)
 		self.hellostring = hellostring
 
 	def hello(self):
 		print '%s: %f' % (self.hellostring,time.time())
 
+	def stop(self,timer_name):
+		print 'stopping timer: %s' % timer_name
+		event.modify_timer(timer_name,interval=0)
+
+	def modify_timer(self,timer_name,new_interval):
+		print 'modify timer: %s' % timer_name
+		event.modify_timer(timer_name,new_interval)
+
 class SocketTest(event.Event):
 	
 	def __init__(self,port=10000):
+		event.Event.__init__(self)
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.sock.bind(('', port))
@@ -21,18 +31,31 @@ class SocketTest(event.Event):
 		self.sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
 	def receive(self):
-		data = self.sock.recv(1024)
-		print data
+		message = self.sock.recv(1024)
+		if message == 'execute':
+			self.emit('test',message)
+		if message.split('_')[0] == 'stop':
+			self.emit('stop',message.split('_')[1])
+		if message.split('_')[0] == 'modifytimer':
+			self.emit('modifytimer',message.split('_')[1],float(message.split('_')[2]))
+			
+	def send(self,message=''):
+		print 'socket send: %s' % message
 
 def main():
 
-	timer1 = TimerTest('timer2')
-	timer2 = TimerTest('timer1')
+	timer1 = TimerTest('timer1')
+	timer2 = TimerTest('timer2')
 	sock1 = SocketTest(10001)
 	sock2 = SocketTest(10002)
 
-	event.add_timer(1,timer1.hello)
-	event.add_timer(3,timer2.hello)
+	sock1.connect('test',sock2.send)
+	sock1.connect('stop',timer1.stop)
+	sock1.connect('stop',timer2.stop)
+	sock1.connect('modifytimer',timer1.modify_timer)
+	sock1.connect('modifytimer',timer2.modify_timer)
+	event.add_timer(1,timer1.hello,name='timer1')
+	event.add_timer(3,timer2.hello,name='timer2')
 	event.add_io_watcher(sock1.sock,sock1.receive)
 	event.add_io_watcher(sock2.sock,sock2.receive)
 
